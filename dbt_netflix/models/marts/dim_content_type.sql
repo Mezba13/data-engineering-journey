6e3f99f8873b9961de6ddde93b68c897b89522d2
Mezba13 <mezbauddin.bsmrstu@gmail.com>
2026-09-06 09:37:37 +0600

    feat: day 12 - dbt project with staging and mart models

---
+++ dbt_netflix/models/marts/dim_content_type.sql
@@ -0,0 +1,8 @@
+-- dim_content_type.sql
+SELECT DISTINCT
+    type as content_type,
+    CASE 
+        WHEN type = 'Movie' THEN 'Film Content'
+        ELSE 'Series Content'
+    END as content_category
+FROM {{ ref('stg_netflix') }}
